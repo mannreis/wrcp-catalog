629d00080874176933eb6ad870b24e6782f8e206
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-05-02 20:48:59 +0200

    index: css-cleanup: more concise + allowed values

--- scripts/render_catalog.py
+++ scripts/render_catalog.py
@@ -21,6 +21,21 @@ def highlight_code(code, lexer_name="python"):
     formatter = HtmlFormatter(cssclass="source")
     return Markup(highlight(dedent(code), lexer, formatter))
 
+def format_default_params(mlds):
+    if descr := mlds.get("parameter_descriptions", {}):
+        return "(" + ", ".join(f"{name}={d['default']!r}" for name, d in descr.items()) + ")"
+    else:
+        return ""
+
+def format_raw_list(l):
+    return ", ".join(f"{e!r}" for e in l)
+
+def max_allowed_params(mlds):
+    return {
+        key: list(sorted(set(a for r in mlds["raw"].values() for a in r["allowed_parameters"][key])))
+        for key in mlds.get("parameter_descriptions", {})
+    }
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -35,13 +50,15 @@ def main():
     )
     env.filters["highlight_css"] = generate_highlight_css
     env.filters["highlight"] = highlight_code
+    env.filters["default_params"] = format_default_params
+    env.filters["raw_list"] = format_raw_list
 
     mldss = json.load(open(args.mlds))
 
     template = env.get_template("index.html")
 
     with open(args.outdir / "index.html", "w") as outfile:
-        outfile.write(template.render(mldss=mldss, sorted=sorted))
+        outfile.write(template.render(mldss=mldss, sorted=sorted, max_allowed_params=max_allowed_params))
 
 if __name__ == "__main__":
     main()
