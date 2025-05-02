47a51b8125bd0641cf3b1c19d4bbd8f5287f0182
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-05-02 12:05:14 +0200

    highlight code snippet

--- scripts/render_catalog.py
+++ scripts/render_catalog.py
@@ -1,7 +1,25 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from markupsafe import Markup
+from textwrap import dedent
 from pathlib import Path
 import argparse
 import json
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+def generate_highlight_css(style="default"):
+    # Generate the CSS using Pygments
+    css = HtmlFormatter(style=style).get_style_defs('.source')
+
+    # Return the CSS as a string
+    return Markup(css)
+
+def highlight_code(code, lexer_name="python"):
+    lexer = get_lexer_by_name(lexer_name)
+    formatter = HtmlFormatter(cssclass="source")
+    return Markup(highlight(dedent(code), lexer, formatter))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,6 +33,8 @@ def main():
         loader=FileSystemLoader(Path(__file__).parent / "templates"),
         autoescape=select_autoescape(),
     )
+    env.filters["highlight_css"] = generate_highlight_css
+    env.filters["highlight"] = highlight_code
 
     mldss = json.load(open(args.mlds))
 
